70e9ec5801ccdce0ab739297fc23018f68014d41
sauravarya <sauravarya22896@gmail.com>
2021-10-31 17:18:51 -0400

    CalculatorLogic shared class library performs the division task

--- CalculatorLogic/Class1.cs
+++ CalculatorLogic/Class1.cs
@@ -21,5 +21,18 @@ namespace CalculatorLogic
         {
             return num1 * num2;
         }
+
+        //this method below in CalculatorLogics shared class library accepts two double data type numbers and return the result by dividing first number by secind number
+        public static double Division(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
     }
 }
