e50ea0c93de933f66e89e00ab7bc537620357c06
sauravarya <sauravarya22896@gmail.com>
2021-10-31 17:10:13 -0400

    CalculatorLogic shared class library performs the subtraction task

--- CalculatorLogic/Class1.cs
+++ CalculatorLogic/Class1.cs
@@ -9,5 +9,11 @@ namespace CalculatorLogic
         {
             return num1 + num2;
         }
+
+        //this method below in CalculatorLogics shared class library accepts two double data type numbers and return the result by subtracting second number from the first number
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
     }
 }
