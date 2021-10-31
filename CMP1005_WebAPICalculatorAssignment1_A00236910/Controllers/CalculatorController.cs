fe8d9626ad33a16957194628f28272bfecb67c07
sauravarya <sauravarya22896@gmail.com>
2021-10-31 17:05:13 -0400

    Calculator shared class library performs the addition task with the addition method

--- CMP1005_WebAPICalculatorAssignment1_A00236910/Controllers/CalculatorController.cs
+++ CMP1005_WebAPICalculatorAssignment1_A00236910/Controllers/CalculatorController.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CalculatorLogic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace CMP1005_WebAPICalculatorAssignment1_A00236910.Controllers
+namespace CMP1005_WebAPIAssignment1_A00236910.Controllers
 {
-    [Route("api/[controller]")]
-    public class CalculatorController : Controller
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //this method accepts two double data type numbers to return their sum from CalculatorLogic shared library addition method
+        //[HttpGet]
+        //public double Add([FromQuery] double num1, [FromQuery] double num2)
+        //{
+        //    return Class1.Addition(num1, num2);
+        //}
     }
 }
--- CalculatorLogic/Class1.cs
+++ CalculatorLogic/Class1.cs
@@ -4,5 +4,10 @@ namespace CalculatorLogic
 {
     public class Class1
     {
+        //this method below in CalculatorLogics shared class library accepts two double data type numbers and return their sum
+        public static double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
     }
 }
