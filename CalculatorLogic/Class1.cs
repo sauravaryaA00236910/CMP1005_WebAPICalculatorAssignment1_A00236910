7c19b3f3ee19c05b3b699dbcfe30184c6175b087
sauravarya <sauravarya22896@gmail.com>
2021-10-31 17:13:26 -0400

    CalculatorLogic shared class library performs the multiplication task

--- CalculatorLogic/Class1.cs
+++ CalculatorLogic/Class1.cs
@@ -15,5 +15,11 @@ namespace CalculatorLogic
         {
             return num1 - num2;
         }
+
+        //this method below in CalculatorLogics shared class library accepts two double data type numbers and return the result by multiplying the two numbers
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
     }
 }
