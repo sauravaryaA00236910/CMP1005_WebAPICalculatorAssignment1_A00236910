03ec1ba2d6240ef7111f4137c4940e69db1a235a
sauravarya <sauravarya22896@gmail.com>
2021-11-01 16:56:06 -0400

    10 successful unit tests are added for Multiplication method

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -276,5 +276,140 @@ namespace CalculatorUnitTest
 
             Assert.AreEqual(desired, actualResult);
         }
+
+
+        //************ Multiplication Tests ****************
+
+        //the below 10 test methods are for Multiplication unit test checks
+
+        //Test 1
+        [TestMethod]
+        public void MultiplicationUnitTestCheck1()
+        {
+            double num1 = 7;
+            double num2 = 4;
+            double desired = 28;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 2
+        [TestMethod]
+        public void MultiplicationUnitTestCheck2()
+        {
+            double num1 = 11;
+            double num2 = 3;
+            double desired = 33;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 3
+        [TestMethod]
+        public void MultiplicationUnitTestCheck3()
+        {
+            double num1 = 15;
+            double num2 = 4;
+            double desired = 60;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 4
+        [TestMethod]
+        public void MultiplicationUnitTestCheck4()
+        {
+            double num1 = 23;
+            double num2 = 11;
+            double desired = 253;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 5
+        [TestMethod]
+        public void MultiplicationUnitTestCheck5()
+        {
+            double num1 = 33;
+            double num2 = 9;
+            double desired = 297;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 6
+        [TestMethod]
+        public void MultiplicationUnitTestCheck6()
+        {
+            double num1 = 17;
+            double num2 = 11;
+            double desired = 187;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 7
+        [TestMethod]
+        public void MultiplicationUnitTestCheck7()
+        {
+            double num1 = 53;
+            double num2 = 7;
+            double desired = 371;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 8
+        [TestMethod]
+        public void MultiplicationUnitTestCheck8()
+        {
+            double num1 = 61;
+            double num2 = 3;
+            double desired = 183;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 9
+        [TestMethod]
+        public void MultiplicationUnitTestCheck9()
+        {
+            double num1 = 77;
+            double num2 = 5;
+            double desired = 385;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
+
+        //Test 10
+        [TestMethod]
+        public void MultiplicationUnitTestCheck10()
+        {
+            double num1 = 88;
+            double num2 = 11;
+            double desired = 968;
+
+            double actualResult = Class1.Multiplication(num1, num2);
+
+            Assert.AreEqual(desired, actualResult);
+        }
     }
 }
